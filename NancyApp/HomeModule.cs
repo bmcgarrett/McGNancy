b252392f6ed577f3cc1df4a5548b04cb3d9a0f1f
unknown <bmcgarrsa@WKS-87928.corp.agp.ads>
2013-09-03 15:41:47 -0400

    First Nancy Commit

---
+++ NancyApp/HomeModule.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace NancyApp
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World from Nancy";
+
+            Get["/tools"] = x =>
+                            {
+                                string toolName = Request.Query["ToolName"];
+                                int toolID = Request.Query["ToolID"];
+                                
+                                var tool = new Tool();
+                                tool.ToolId = toolID;
+                                tool.ToolName = toolName;
+
+                                return View["Tool.html", tool];
+                            };
+        }
+    }
+}
---
+++ NancyApp/Models/Tool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyApp
+{
+    public class Tool
+    {
+        public int ToolId { get; set; }
+
+        public string ToolName { get; set; }
+    }
+}
