067d1497f14026b53296307214eff30ca1e81e10
Brendan McGarrett <bmcgarrett@gmail.com>
2013-10-26 09:31:51 -0400

    Added route test for employees

--- NancyTests/HomeModuleTest.cs
+++ NancyTests/HomeModuleTest.cs
@@ -13,7 +13,7 @@ namespace NancyTests
     public class HomeModuleTest
     {
         [Test]
-        public void Should_return_status_ok_when_route_exists()
+        public void Should_return_status_ok_when_root_route_exists()
         {
             // Given
             var boostrapper = new ConfigurableBootstrapper(with =>
@@ -31,8 +31,27 @@ namespace NancyTests
 
             // Then
             Assert.AreEqual(HttpStatusCode.OK,result.StatusCode);
+        }
 
+        [Test]
+        public void Should_return_status_ok_when_Employees_route_exists()
+        {
+            // Given
+            var boostrapper = new ConfigurableBootstrapper(with =>
+            {
+                with.Module<HomeModule>();
+            });
 
+            var browser = new Browser(boostrapper);
+
+            // When
+            var result = browser.Get("/Employees", with =>
+            {
+                with.HttpRequest();
+            });
+
+            // Then
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode); 
         }
     }
 }
