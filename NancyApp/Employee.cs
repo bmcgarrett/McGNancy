aa38d5445222fdd1e8b794bc0d3f6bd13c783bec
Brendan McGarrett <bmcgarrett@hotmail.com>
2013-12-12 14:42:45 -0500

    Updated with working incrementer

--- NancyApp/Employee.cs
+++ NancyApp/Employee.cs
@@ -7,8 +7,21 @@ namespace NancyApp
 {
     public class Employee
     {
+        public static int Id = 0;
+
+        public int empIDCount;
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public Employee()
+        {
+            Id++;
+            empIDCount = Id;
+        }
+
+        public int EmployeeID
+        {
+            get { return empIDCount; }
+        }
     }
 }
